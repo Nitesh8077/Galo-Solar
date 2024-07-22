3cdebf3482908c99b8730dee01bf824badcd8000
rohit-vue <rohit.excel2011@gmail.com>
2024-07-22 11:51:59 +0530

    update#form

--- src/About/ContactForm.jsx
+++ src/About/ContactForm.jsx
@@ -1,8 +1,18 @@
 import React, { useEffect, useState, useRef } from "react";
+import emailjs from "emailjs-com";
 import "tailwindcss/tailwind.css";
 
 const ContactForm = () => {
   const [isVisible, setIsVisible] = useState(false);
+  const [formData, setFormData] = useState({
+    from_name: "",
+    address: "",
+    phone: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
   const containerRef = useRef(null);
 
   useEffect(() => {
@@ -30,6 +40,33 @@ const ContactForm = () => {
     };
   }, []);
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .send(
+        "service_yc9r8to", // Replace with your EmailJS service ID
+        "template_sh6azhy", // Replace with your EmailJS template ID
+        formData,
+        "0nQcDotTo-9_-pFPG" // Replace with your EmailJS user ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Email sent successfully");
+        },
+        (error) => {
+          console.error(error.text);
+          alert("There was an error submitting the form.");
+        }
+      );
+  };
+
   return (
     <div
       ref={containerRef}
@@ -48,39 +85,58 @@ const ContactForm = () => {
         </p>
       </div>
       <form
+        onSubmit={handleSubmit}
         className={`mt-8 grid grid-cols-1 gap-4 sm:grid-cols-2 transition-opacity duration-1000 ${
           isVisible ? "opacity-100" : "opacity-0"
         }`}
       >
         <input
           type="text"
+          name="from_name"
           placeholder="Name"
           className="p-4 border border-gray-300 rounded-md w-full sm:col-span-1"
+          value={formData.from_name}
+          onChange={handleChange}
         />
         <input
           type="text"
-          placeholder="Company"
+          name="address"
+          placeholder="Address"
           className="p-4 border border-gray-300 rounded-md w-full sm:col-span-1"
+          value={formData.address}
+          onChange={handleChange}
         />
         <input
           type="tel"
+          name="phone"
           placeholder="Phone"
           className="p-4 border border-gray-300 rounded-md w-full sm:col-span-1"
+          value={formData.phone}
+          onChange={handleChange}
         />
         <input
           type="email"
+          name="email"
           placeholder="Email"
           className="p-4 border border-gray-300 rounded-md w-full sm:col-span-1"
+          value={formData.email}
+          onChange={handleChange}
         />
         <input
           type="text"
+          name="subject"
           placeholder="Subject"
           className="p-4 border border-gray-300 rounded-md w-full sm:col-span-2"
+          value={formData.subject}
+          onChange={handleChange}
         />
         <textarea
+          name="message"
           placeholder="Message"
           className="p-4 border border-gray-300 rounded-md w-full sm:col-span-2"
           rows="5"
+          value={formData.message}
+          onChange={handleChange}
         ></textarea>
         <div className="sm:col-span-2">
           <button
