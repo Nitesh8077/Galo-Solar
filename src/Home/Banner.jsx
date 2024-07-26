12eb35c44ef395368c13f8a7392c366f4c51add4
rohit-vue <rohit.excel2011@gmail.com>
2024-07-26 15:25:51 +0530

    #h1

--- src/Home/Banner.jsx
+++ src/Home/Banner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="flex justify-center items-center h-32 md:h-32 bg-black p-4 md:p-8">
+    <div className="flex justify-center items-center h-32 md:h-20 bg-black p-4 md:p-8">
       <h1 className="text-yellow-500 text-2xl md:text-4xl lg:text-6xl font-bold text-center">
         Proudly Made in India
       </h1>
