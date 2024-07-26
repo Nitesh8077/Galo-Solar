b90bbf0a241bd78141189a8eb71237255d777920
rohit-vue <rohit.excel2011@gmail.com>
2024-07-26 15:26:53 +0530

    #22

--- src/Home/Banner.jsx
+++ src/Home/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div className="flex justify-center items-center h-32 md:h-20 bg-black p-4 md:p-8">
       <h1 className="text-yellow-500 text-2xl md:text-4xl lg:text-6xl font-bold text-center">
-        Proudly Made in India
+        Proudly Made in India1
       </h1>
     </div>
   );
