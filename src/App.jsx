c7dd4af154fe2ab38fedeebd2b7abbd3a60f375a
rohit-vue <rohit.excel2011@gmail.com>
2024-07-30 15:25:56 +0530

    #wpUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Navigation from "./customer/component/Navigation/Navigation";
+import FloatingButton from "./FloatingButton/FloatingButton";
 export default function App() {
   return (
     <>
       <Navigation />
+      <FloatingButton />
     </>
   );
 }
---
+++ src/FloatingButton/FloatingButton.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import whatsappIcon from "/images/wp.png"; // Update this path
+
+const FloatingButton = () => {
+  const phoneNumber = "+919311797244";
+  const message =
+    "Hello, I came from your website :: https://galo.co.in/ How are you?";
+
+  const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+    message
+  )}`;
+
+  return (
+    <div className="fixed right-0 top-1/2 transform -translate-y-1/2 z-50">
+      <a href={whatsappLink} target="_blank" rel="noopener noreferrer">
+        <img src={whatsappIcon} alt="Reach us Out" className="h-5 md:h-10" />
+      </a>
+    </div>
+  );
+};
+
+export default FloatingButton;
