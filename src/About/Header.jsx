743b047cccea582f51a1967e7562000f311ffd32
rohit-vue <rohit.excel2011@gmail.com>
2024-08-01 13:45:46 +0530

    #html

--- src/About/Header.jsx
+++ src/About/Header.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <div>
-        <img src="/images/ABT.png" />
-      </div>
+    <div className="relative w-full h-screen overflow-hidden">
+      <img
+        src="/images/ABT.png"
+        className="absolute top-0 left-0 w-full h-1/2 object-cover"
+        alt="Header"
+      />
     </div>
   );
 };
