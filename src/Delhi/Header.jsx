45e94f033a0641ea286ed51cddc130bf5d3972b9
galo solar <galosolar815@gmail.com>
2024-10-08 15:05:49 +0530

    #newfileupload

--- src/Delhi/Header.jsx
+++ src/Delhi/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <img src="images/lanban.png" className="w-full" />
+      <img src="images/panel3.jpg" className="w-full" />
     </div>
   );
 };
